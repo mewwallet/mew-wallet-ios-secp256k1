aced666be462b61d2fdf4a01e6966251c097e925
Misha Nikanorov <fobozsb@gmail.com>
2022-06-19 01:22:32 -0700

    develop: Update dependency; Rename Package

--- Tests/mew-wallet-ios-secp256k1-tests/mew_wallet_ios_secp256k1_tests.swift
+++ Tests/mew-wallet-ios-secp256k1-tests/mew_wallet_ios_secp256k1_tests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import MEW_wallet_iOS_secp256k1_package
+@testable import mew_wallet_ios_secp256k1
 
-final class MEW_wallet_iOS_secp256k1_packageTests: XCTestCase {
+final class mew_wallet_ios_secp256k1_tests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
